40b48023e2f97bb910260c4a6b776aaa1411af09
Jenom <119628596+shehuj@users.noreply.github.com>
2025-10-19 00:53:37 -0500

    Update analyze_image.py

--- analyze_image.py
+++ analyze_image.py
@@ -31,7 +31,7 @@ def main():
     # Get environment variables
     aws_region = os.getenv("AWS_REGION")
     s3_bucket = os.getenv("S3_BUCKET")
-    branch_name = os.getenv("GITHUB_REF_NAME", "dev", "main")
+    branch_name = os.getenv("GITHUB_REF_NAME", "dev").replace("/", "-")
     aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
     aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 
